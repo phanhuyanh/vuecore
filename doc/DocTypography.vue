93ab0ba5c2b606726c4d6c9d5f120f75a7c99d9e
huyme <phanhuyanh251@gmail.com>
2021-05-27 21:55:36 +0700

    typography

--- doc/DocTypography.vue
+++ doc/DocTypography.vue
@@ -2,6 +2,49 @@
   <div class="customize-typography doc">
     <div class="doc-item">
       <h2 id="display">Display</h2>
+      <div class="typo-content">
+        <h5 class="vc-display-1">Display 1</h5>
+        <h5 class="vc-display-2">Display 2</h5>
+      </div>
+    </div>
+    <div class="doc-item">
+      <h2 id="headline">Headline</h2>
+      <div class="typo-content">
+        <h5 class="vc-headline-1">Healine 1</h5>
+        <h5 class="vc-headline-2">Healine 2</h5>
+        <h5 class="vc-headline-3">Healine 3</h5>
+        <h5 class="vc-headline-4">Healine 4</h5>
+        <h5 class="vc-headline-5">Healine 5</h5>
+        <h5 class="vc-headline-6">Healine 6</h5>
+      </div>
+    </div>
+    <div class="doc-item">
+      <h2 id="lead">Lead</h2>
+      <div class="typo-content">
+        <h5 class="vc-lead">Silver mist suffused the deck of the ship.</h5>
+      </div>
+    </div>
+    <div class="doc-item">
+      <h2 id="body">Body</h2>
+      <div class="typo-content">
+        <div class="vc-text-body w-50p">
+          By the same illusion which lifts the horizon of the sea to the level
+          of the spectator on a hillside, the sable cloud beneath was dished
+          out, and the car seemed to float in the middle of an immense dark
+          sphere, whose upper half was strewn with silver. Looking down into the
+          dark gulf below, I could see a ruddy light streaming through a rift in
+          the clouds.
+        </div>
+      </div>
+    </div>
+    <div class="doc-item">
+      <h2 id="blockqoute">Blockquote</h2>
+      <div class="typo-content">
+        <div class="vc-blockquotes w-50p">
+          Apparently we had reached a great height in the atmosphere, for the
+          sky was a dead black, and the stars had ceased to twinkle.
+        </div>
+      </div>
     </div>
   </div>
 </template>
